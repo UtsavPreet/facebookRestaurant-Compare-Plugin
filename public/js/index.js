7d948beb54ee177fc9fe34ff01ff4b21acda54ea
UtsavPreet <utsavpreet@gmail.com>
2017-08-05 13:56:49 +0530

    Added MongoDB and saved data to DB

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,18 @@
 var searchData = {};
-var fetchedData =[];
-$('document').ready(function() {
+var x = {};
+var fetchedData = [];
+$('document').ready(function () {
     makeTemplates();
-    
+
 })
-    bind('.button', function () {
-        console.log("Submit");
-        searchData.name = $('.restaurantName').val().trim();
-        execute('fetchData',searchData,function(data){
-            console.log(data);
-            rb('.mainContainer','restaurants',data)
-        });
-    });
-   
-
-
-   
+bind('.button', function () {
+    console.log("Submit");
+    searchData.name = $('.restaurantName').val().trim();
+     execute('fetchData',searchData);
+    // execute('dbData', searchData, function (data) {
+    //     x.dbData=data;
+    //     console.log(x);
+    //     rb('.mainContainer', 'restaurants', x)
+    // })
+    
+});
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var port = process.env.PORT || 8080;
 var bodyParser = require('body-parser');
 var request = require('request');
 var router = express.Router();
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/restaurant';
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({
     extended: true
@@ -13,14 +15,22 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/views/index.html');
 });
+mongo.connect(url, function (err, db) {
+    if (err) {
+        console.log(err);
+    }
+    console.log("connected");
+    global.db = db;
+});
 global.restaurantData;
+global.findResult;
 app.post('/fetchData', function (req, res) {
     console.log(req.body);
-    const pageFieldSet = 'name,posts.limit(2){name,actions,likes.summary(true),total_count},checkins,engagement,fan_count,restaurant_services,restaurant_specialties,overall_star_rating,events.limit(1){name,attending_count,declined_count,interested_count,description,cover}';
+    const pageFieldSet = 'name,talking_about_count,posts.limit(1){name,likes.summary(true),total_count,},checkins,engagement,fan_count,overall_star_rating,events.limit(1){name,attending_count,declined_count,interested_count,description,cover}';
     var searchType = 'page';
     const options = {
         method: 'GET',
-        uri: 'https://graph.facebook.com/v2.10/search',
+        uri: 'https://graph.facebook.com/v2.10/search/',
         qs: {
             access_token: '778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM',
             q: req.body.name,
@@ -30,11 +40,24 @@ app.post('/fetchData', function (req, res) {
     };
 
     request(options, function (err, resp, body) {
-        global.restaurantData = JSON.parse(body);
-        console.log(global.restaurantData);
-        res.json(global.restaurantData);
+        // global.restaurantData = JSON.parse(body);
+        // console.log(global.restaurantData);
+        // res.json(global.restaurantData);
+        res.json(JSON.parse(body));
+        global.db.collection('restaurantData').insert({
+            data: JSON.parse(body)
+        });
+
     })
 
+})
+app.post('/dbData', function (req, res) {
+    global.db.collection('restaurantData').find({}).toArray(function (err, result) {
+        if (err) throw err;
+        global.findResult = result;
+        res.send(global.findResult);
+    })
+    
 })
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);

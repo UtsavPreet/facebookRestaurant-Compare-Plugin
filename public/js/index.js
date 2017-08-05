768bb62fd807dc73ca9f4cc14a676bb80d89a0fb
UtsavPreet <utsavpreet@gmail.com>
2017-08-05 17:48:40 +0530

    Merged Posts Requests

--- public/js/index.js
+++ public/js/index.js
@@ -8,17 +8,9 @@ $('document').ready(function () {
 bind('.button', function () {
     console.log("Submit");
     searchData.name = $('.restaurantName').val().trim();
-    // execute('fetchData',searchData);
-     // execute('posts',searchData);
-    execute('posts',searchData,function(dataGot){
-            console.log(dataGot);
-            rb('.reviewContainerBlock','post',dataGot);
-        })
-    execute('dbData', searchData, function (data) {
-        x.dbData=data;
-        console.log(x);
-        rb('.mainContainer', 'restaurants', x);
-    })
-    
-    
+    execute('fetchData', searchData, function (data) {
+    x.dbData = data;
+    console.log(x);
+    rb('.mainContainer', 'restaurants', x);
+})
 });
--- server.js
+++ server.js
@@ -41,76 +41,27 @@ app.post('/fetchData', function (req, resp) {
         console.log(res);
         global.restaurantData = res;
         global.db.collection('restaurantData').insert({
-        data: global.restaurantData
+            _id: req.body.name,
+            data: global.restaurantData
+        });
     });
-    });
-    console.log(global.restaurantData);    
-})
-app.post('/posts',function(req,resp){
-    graph.get(req.body.name+"/posts?since= 2017-07-01&&until= now",function(err,res){
+    console.log(global.restaurantData);
+    graph.get(req.body.name + "/posts?since= 2017-07-01&&until= now", function (err, res) {
         global.posts = res;
         console.log(global.posts);
-        global.db.collection('restaurantData').insert({
-            posts: res
+        global.db.collection('restaurantData').update({
+            _id: req.body.name
+        }, {
+            $set: {
+                posts: res
+            }
         })
-        resp.send(global.posts);
-    })    
-})
-// app.post('/fetchData', function (req, res) {
-//     console.log(req.body);
-//     const pageFieldSet = 'talking_about_count,name,posts.limit(1){name,likes.summary(true),total_count},checkins,engagement,fan_count,overall_star_rating,events.limit(1){name,attending_count,declined_count,interested_count,description,cover}';
-//     var searchType = 'page';
-//     const options = {
-//         method: 'GET',
-//         uri: 'https://graph.facebook.com/v2.10/search/',
-//         qs: {
-//             access_token: '778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM',
-//             q: req.body.name,
-//             type: 'page',
-//             fields: pageFieldSet
-//         }
-//     };
-
-//     request(options, function (err, resp, body) {
-//         // global.restaurantData = JSON.parse(body);
-//         // console.log(global.restaurantData);
-//         // res.json(global.restaurantData);
-//         res.json(JSON.parse(body));
-//         global.db.collection('restaurantData').insert({
-//             data: JSON.parse(body)
-//         });
-
-//     })
-
-// })
-// app.post('/posts',function(req,res){
-//     const pageFieldSet = 'posts?since= 2017-07-01&&until= now';
-//     var searchType = 'page'
-//     const options = {
-//         method: 'GET',
-//         uri: 'https://graph.facebook.com/v2.10/${req.body.name}',
-//         qs: {
-//             access_token: '778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM',
-//             fields: pageFieldSet
-//         }
-//     };
-//     request(options, function (err, resp, body) {
-//         // global.restaurantData = JSON.parse(body);
-//         // console.log(global.restaurantData);
-//         // res.json(global.restaurantData);
-//         res.json(JSON.parse(body));
-//         global.db.collection('restaurantData').insert({
-//             data: JSON.parse(body)
-//         });
-// })
-// })
-app.post('/dbData', function (req, res) {
+    })
     global.db.collection('restaurantData').find({}).toArray(function (err, result) {
         if (err) throw err;
         global.findResult = result;
-        res.send(global.findResult);
+        resp.send(global.findResult);
     })
-
 })
 app.listen(port);
 console.log('Server started! At http://localhost:' + port);

de8fe1b17347b5da1f9f124cd01dca5c642c00b7
UtsavPreet <utsavpreet@gmail.com>
2017-08-10 16:19:40 +0530

    Updated API

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,94 @@
 var searchData = {};
 var x = {};
 var fetchedData = [];
-$('document').ready(function () {
+var z = {};
+z['restaurants'] = [];
+$('document').ready(function() {
     makeTemplates();
 
 })
-bind('.button', function () {
+bind('.button', function() {
     console.log("Submit");
     searchData.name = $('.restaurantName').val().trim();
-    execute('fetchData', searchData, function (data) {
-        x.dbData = data;
-        console.log(x);
-        rb('.mainContainer', 'restaurants', x);
-        bind('.addButton', function () {
-            $('.addRestaurantContainer').show();
-            rb('.addRestaurantContainer', 'addRestaurant', {});
-            bind('.add', function () {
-                searchData.name = $('.addRestaurant').val().trim();
-                execute('fetchData', searchData, function (data) {
-                    x.dbData = data;
-                    console.log(x);
-                    rb('.mainContainer', 'restaurants', x);
-                })
-            })
-        })
+
+    execute('fetchData', searchData, function(data) {
+        // if (data.length != 0)
+        // data.totalEvent = (data.events.eventDetail.length);
+        console.log(data);
+        z.restaurants.push(data);
+        displayData(z)
     })
-    // requestdata(searchData);
-    // bind('.addButton', function () {
-    //     $('.addRestaurantContainer').show();
-    //     rb('.addRestaurantContainer', 'addRestaurant', {});
-    //     bind('.add', function () {
-    //         searchData.name = $('.addRestaurant').val().trim();
-    //         execute('fetchData', searchData, function (data) {
-    //         x.dbData = data;
-    //         console.log(x);
-    //         rb('.mainContainer', 'restaurants', x);
-    //     })
-    //     })
+
+
+    // execute('restaurantDetail', searchData, function(data) {
+    //     // data.totalEvent = (data.events.eventDetail.length);
+
+    //     // z.restaurants.push(data);
+    //     // console.log(z);
+    //     var y = {};
+    //     y.restaurants = data;
+    //     displayData(y)
     // })
 })
-// function requestdata(search) {
 
-// }
+function displayData(data) {
+    console.log(data);
+
+
+    z = (data);
+
+
+    rb('.mainContainer', 'restaurants', z, {}, '.events', function(el, x) {
+        console.log(x.restaurantId);
+        var y = {};
+        y.name = x.restaurantId
+        execute('eventDetails', y, function(eventsData) {
+            console.log(eventsData);
+            $('.eventContainerBlock').show();
+            rb('.eventContainerBlock', 'event', eventsData, {}, '.eventOverlay', function() {
+                $('.eventContainerBlock').hide();
+            })
+        })
+    });
+
+    bind('.post', function(data) {
+        var y = {};
+        // y.name = '';
+        if (typeof $(this).data('id') != "string")
+            y.name = JSON.stringify($(this).data('id'));
+        else
+            y.name = $(this).data('id')
+        console.log(y);
+        execute('postDetails', y, function(postDetails) {
+            // console.log(postDetails);
+            $('.eventContainerBlock').show();
+            rb('.eventContainerBlock', 'post', postDetails, {}, '.eventOverlay', function() {
+                $('.eventContainerBlock').hide();
+            })
+
+        })
+    })
+
+
+
+    bind('.addButton', function() {
+        $('.addRestaurantContainer').show();
+        rb('.addRestaurantContainer', 'addRestaurant', {}, {}, '.overlay', function() {
+            $('.addRestaurantContainer').hide();
+        });
+        bind('.add', function() {
+            $('.addRestaurantContainer').hide();
+
+            searchData.name = $('.addRestaurant').val().trim();
+            if (searchData.name) {
+                execute('fetchData', searchData, function(data) {
+                    // var p = {};
+                    // p.restaurant = data
+                    z.restaurants.push(data);
+                    console.log(data);
+                    displayData(z);
+                })
+            }
+        })
+    })
+}
--- server.js
+++ server.js
@@ -1,98 +1,304 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8080;
-var graph = require('fbgraph');
-var bodyParser = require('body-parser');
-var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-var async = require('async');
-var url = 'mongodb://localhost:27017/restaurant';
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({
-    extended: true
-})); // support encoded bodies
-var path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/views/index.html');
-});
-mongo.connect(url, function (err, db) {
-    if (err) {
-        console.log(err);
-    }
-    console.log("connected");
-    global.db = db;
-});
-global.restaurantData;
-global.findResult;
-global.posts;
-global.eventIds = [];
-global.eventData = [];
-global.pageID;
-graph.setAccessToken('778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM');
-var options = {
-    timeout: 3000,
-    pool: {
-        maxSockets: Infinity
-    },
-    headers: {
-        connection: "keep-alive"
-    }
-};
-graph.setVersion("2.10");
-app.post('/fetchData', function (req, resp) {
-    global.pageID = req.body.name;
-    graph.get(req.body.name + "?fields=name,talking_about_count,checkins,engagement,fan_count,overall_star_rating", function (err, res) {
-        global.restaurantData = res;
-        global.db.collection('restaurantData').save({
-            _id: req.body.name,
-            data: global.restaurantData
-        });
-    });
-    graph.get(req.body.name + "/posts?since= 2017-07-01&&until= now", function (err, res) {
-        global.posts = res;
+ var express = require('express');
+ var app = express();
+ var port = process.env.PORT || 8080;
+ var fb = require("fb")
+ var bodyParser = require('body-parser');
+ var mongo = require('mongodb').MongoClient;
+ var async = require('async');
+ var url = 'mongodb://localhost:27017/restaurent';
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({
+     extended: true
+ }));
+ var path = require('path');
+ app.use(express.static(path.join(__dirname, 'public')));
+ app.get('/', function(req, res) {
+     res.sendFile(__dirname + '/public/views/index.html');
+ });
+ mongo.connect(url, function(err, db) {
+     if (err) {
+         console.log(err);
+     }
+     console.log("connected");
+     global.db = db;
+ });
+ fb.setAccessToken('778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM');
+ // global.restaurantData;
+ // global.findResult;
+ // // global.posts;
+ // global.eventIds = [];
+ // global.eventData = [];
+ global.pageID;
+ // graph.setAccessToken('778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM');
+ // var options = {
+ //     timeout: 3000,
+ //     pool: {
+ //         maxSockets: Infinity
+ //     },
+ //     headers: {
+ //         connection: "keep-alive"
+ //     }
+ // };
+ // graph.setVersion("2.10");
 
-        global.db.collection('restaurantData').update({
-            _id: req.body.name
-        }, {
-            $set: {
-                posts: res
-            }
-        })
-        graph.get(req.body.name + "/events?since= 2017-07-01&&until= now", function (err, res) {
-            global.posts = res;
-            var counter = 0;
-            var detail;
+ // app.post('/restaurantDetail', function(req, res) {
+ //     var a = {};
+ //     a['restaurants'] = [];
+ //     global.db.collection('competitors').find({ restaurantName: req.body.name }).toArray(function(err, result) {
+ //         if (err)
+ //             throw err;
+ //         else {
+ //             async.forEachSeries(result[0].competitor, function(item, callback) {
+ //                 if (item.facebookName != undefined) {
+ //                     getDetail(item.facebookName, function(r1) {
+ //                         a.restaurants.push(r1);
+ //                         callback();
+ //                     });
+ //                 } else
+ //                     callback();
 
-            async.forEachSeries(res.data, function (item, callback) {
-                var eventID = item.id;
-                graph.get(eventID + "?fields=name,description,attending_count,can_guests_invite,can_viewer_post,noreply_count,declined_count,interested_count,maybe_count", function (err, result) {
-                    global.eventData.push(result);
-                    callback();
-                })
+ //             }, function() {
+ //                 res.send(a);
+ //             })
+ //         }
 
-            }, function () {
-                // sendDBData();
-                global.db.collection('restaurantData').update({
-                    _id: global.pageID
-                }, {
-                    $set: {
-                        events: global.eventData
-                    }
-                })
-                global.db.collection('restaurantData').find({}).toArray(function (err, result) {
-                    if (err) throw err;
-                    global.findResult = result;
-                    resp.send(global.findResult);
+ //     })
 
-                })
-            })
-        })
-    })
-})
-app.listen(port);
-console.log('Server started! At http://localhost:' + port);
+ //     function getDetail(competitorName, cb) {
+ //         global.db.collection('facebookRestaurantData').find({ "restaurant.name": competitorName }).toArray(function(err, result) {
+ //             return cb(result[0])
+ //         })
+ //     }
 
-function sendDBData() {
 
-}
+
+ //     // global.db.collection('facebookRestaurantData').find({}).toArray(function(err, result) {
+ //     //     if (err)
+ //     //         throw err;
+ //     //     res.send(result);
+ //     // })
+ // })
+
+ // app.post('/fetchData', function(req, resp) {
+ //     global.pageID = req.body.name;
+
+
+ //     // global.db.collection('facebookRestaurantData').find(function(error, cursor) {
+ //     //     if (error || !cursor) {
+ //     //         console.log("error getting items");
+ //     //     } else {
+ //     //         cursor.toArray(function(error, items) {
+ //     //             if (error || !items) {
+ //     //                 console.log("error getting items");
+ //     //             } else {
+ //     //                 resp.send(JSON.stringify(items));
+ //     //             }
+ //     //         });
+ //     //     };
+ //     // })
+ app.post('/fetchData', function(req, resp) {
+     global.pageID = req.body.name;
+     fb.api('', 'post', {
+         batch: [{
+                 method: 'get',
+                 relative_url: global.pageID + '?fields=name_with_location_descriptor,location,talking_about_count,checkins,fan_count,overall_star_rating,about,cover,feed{name,id,created_time,likes.limit(0).summary(true),comments.limit(0).summary(true),message.limit(0).summary(true),reactions.limit(0).summary(true),status_type},events.limit(10){name,attending_count,cover,declined_count,start_time,interested_count}&since=2017-08-06&until=2017-08-07'
+             },
+
+         ]
+     }, function(res) {
+
+         res0 = JSON.parse(res[0].body);
+         res0.feed.totalPost = res0.feed.data.length;
+         res0.events.totalEvents = res0.events.data.length;
+         res0.restaurantId = global.pageID;
+         global.db.collection('facebookRestaurantData').save({
+             _id: req.body.name,
+             restaurant: res0,
+         })
+         resp.send(res0)
+
+     });
+
+
+
+
+
+     //  fb.api(global.pageID, {
+     //      fields: ['id', 'name', 'fan_count']
+     //  }, function (res) {
+     //      if (!res || res.error) {
+     //          console.log(!res ? 'error occurred' : res.error);
+     //          return;
+     //      }
+     //      console.log(res.id);
+     //      console.log(res.name);
+     //      console.log(res.fan_count);
+     //  });
+ })
+
+
+
+
+
+ //     graph.get(req.body.name + "?fields=name,talking_about_count,checkins,engagement,fan_count,overall_star_rating", function(err, res) {
+ //         global.restaurantData = res;
+
+ //     });
+ //     graph.get(req.body.name + "/posts?since= 2017-07-01&&until= now", function(err, res) {
+ //         global.restaurantData.posts = res;
+ //         // var b = {};
+ //         // b['data'] = [];
+ //         // async.forEachSeries(res.data, function(item, callback) {
+ //         //     var postID = item.id;
+ //         //     graph.get(postID + "?fields=likes.summary(true)", function(err, result) {
+ //         //         //         b.data.push(result);
+ //         //         res.data = result;
+ //         //         // res.data
+ //         //         callback();
+ //         // })
+ //         // }, function() {
+ //         //     // global.db.collection('facebookRestaurantData').update({
+ //         //     //     _id: req.body.name
+ //         //     // }, {
+ //         //     //     $set: {
+ //         //     //         posts: global.postData
+ //         //     //     }
+ //         //     // })
+ //         //     global.restaurantData.post = b;
+ //         // global.restaurantData.posts = res;
+
+ //         eventData();
+ //         //     // res1.send(b);
+
+ //         // })
+
+ //     })
+
+ //     function eventData() {
+ //         graph.get(req.body.name + "/events?since= 2017-07-01&&until= now", function(err, res) {
+
+
+
+ //             var a = {};
+ //             a['eventDetail'] = [];
+ //             async.forEachSeries(res.data, function(item, callback) {
+ //                 var eventID = item.id;
+ //                 graph.get(eventID + "?fields=name,description,attending_count,can_guests_invite,can_viewer_post,declined_count,interested_count,maybe_count,start_time", function(err, result) {
+ //                     result.start_time = result.start_time.substr(0, 10);
+ //                     result.restaurantId = global.pageID;
+ //                     a.eventDetail.push(result);
+ //                     callback();
+ //                 })
+
+ //             }, function() {
+ //                 global.restaurantData.events = a;
+ //                 global.restaurantData.totalEvent = res.data.length;
+
+
+ //                 console.log(global.restaurantData.name);
+ //                 global.db.collection('competitors').update({ 'restaurantName': req.body.competitor }, { '$push': { 'competitor': { 'facebookName': global.restaurantData.name } } });
+
+
+ //                 global.db.collection('facebookRestaurantData').save({
+ //                     _id: req.body.name,
+ //                     restaurant: global.restaurantData
+ //                 })
+ //                 resp.send(global.restaurantData);
+
+ //             })
+ //         })
+
+ //     }
+
+
+ // })
+
+
+ app.post('/eventDetails', function(req, res1) {
+
+     global.db.collection('facebookRestaurantData').find({ _id: req.body.name }).toArray(function(err, result) {
+         if (err)
+             throw err;
+         res1.send(result[0].restaurant.events);
+     })
+
+     //     // var eventsData = {};
+     //     // graph.get(req.body.name + "/events?since= 2017-07-01&&until= now", function(err, res) {
+     //     //     var a = {};
+     //     //     a['eventDetail'] = [];
+     //     //     async.forEachSeries(res.data, function(item, callback) {
+     //     //         var eventID = item.id;
+     //     //         graph.get(eventID + "?fields=name,description,attending_count,can_guests_invite,can_viewer_post,declined_count,interested_count,maybe_count,start_time", function(err, result) {
+     //     //             a.eventDetail.push(result);
+     //     //             callback();
+     //     //         })
+
+     //     //     }, function() {
+     //     //         db.collection('facebookRestaurantData').update({
+     //     //             _id: 'circuscyberhub'
+     //     //         }, {
+     //     //             '$push': { 'events.eventDetail': res }
+     //     //         })
+     //     //         res1.send(a);
+
+     //     //     })
+     //     // })
+
+
+ })
+
+ // app.post('/postDetails', function(req, res) {
+ //     // graph.get(req.body.name + "/posts?since= 2017-07-01&until= now", function(error, result) {
+ //     //     res.send(result);
+
+ //     // })
+
+
+ //     graph.get(req.body.name + "/posts?since= 2017-07-01&until= now", function(err, res) {
+ //         // global.posts = res;
+ //         var b = {};
+ //         b['data'] = [];
+ //         async.forEachSeries(res.data, function(item, callback) {
+ //             var postID = item.id;
+ //             graph.get(postID + "?fields=message,likes.summary(true)", function(err, result) {
+ //                 b.data.push(result);
+ //                 callback();
+ //             })
+ //         }, function() {
+ //             // global.db.collection('facebookRestaurantData').update({
+ //             //     _id: req.body.name
+ //             // }, {
+ //             //     $set: {
+ //             //         posts: global.postData
+ //             //     }
+ //             // })
+ //             // eventData();
+ //             res1.send(b);
+ //         })
+ //     })
+ // })
+
+ // var pages = ['trendkitchen.in','circuscyberhub','cafedelhiheights'];
+
+ // function doSomething(){
+ //     getPosts(pages).then(function(res){
+ //         console.log(res);
+ //     });
+ // }
+
+ // this.getPosts = function (pages) {
+ //         return new Promise(function (res, rej) {
+ //             //get all settings for inventory
+ //             // execute('getSettings', r).then(function (resp) {
+ //             //      (resp) ? res(resp) : res(false);
+ //             // });
+ //             //trendkitchen.in/posts?fields=id,message,shares,likes.limit(0).summary(true),created_time,comments.limit(0).summary(true),reactions.limit(0).summary(true)&limit=2
+ //             graph.get(postID + "?fields=message,likes.summary(true)", function(err, result) {
+ //                 console.log(result)
+ //                 res(result);
+ //             })
+ //         });
+
+ //     }
+ app.listen(port);
+ console.log('Server started! At http://localhost:' + port);

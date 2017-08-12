f927d1150d5da46c5adf44677e04aaea8220e922
UtsavPreet <utsavpreet@gmail.com>
2017-08-12 18:41:29 +0530

    Redesigned the App

---
+++ public/js/data.js
@@ -0,0 +1,71 @@
+var data = {
+    restaurantData: [{
+            name: "Farzi Cafe",
+            picture: "https://scontent.xx.fbcdn.net/v/t1.0-1/c0.0.50.50/p50x50/10615557_519351411543616_347659497323279819_n.jpg?oh=de8e3c7346925e84ab49ba387ccf4057&oe=59EF80BB",
+            rating: "4.8",
+            fanCount: "48K",
+            totalEvent: {
+                eventsTotalCount: "15",
+                eventsAttending: "50",
+            },
+            events: [{
+                name: "Event 1",
+                description: "Demo Description",
+                guestsCount: "50"
+            }],
+            totalPost: {
+                postsTotalCount: "25",
+                postsTotalLikes: "50",
+                postsTotalComments: "50",
+                postsTotalShares: "50",
+                postsTotalReactions:"60"
+            },
+            zomato: {
+                dummyReviewText: "Good Restaurant",
+                totalReviews: "411",
+            },
+            tripAdvisor: {
+                rank: "54",
+                totalReview:"112",
+                positiveReview: "100",
+                negativeReview: "12"
+            }
+
+        },
+        {
+            name: "Farzi Cafe",
+            picture: "https://scontent.xx.fbcdn.net/v/t1.0-1/c0.0.50.50/p50x50/10615557_519351411543616_347659497323279819_n.jpg?oh=de8e3c7346925e84ab49ba387ccf4057&oe=59EF80BB",
+            rating: "4.8",
+            fanCount: "48K",
+            totalEvent: {
+                eventsTotalCount: "15",
+                eventsAttending: "50",
+            },
+            events: [{
+                name: "Event 1",
+                description: "Demo Description",
+                guestsCount: "50"
+            }],
+            totalPost: {
+                postsTotalCount: "25",
+                postsTotalLikes: "50",
+                postsTotalComments: "50",
+                postsTotalShares: "50",
+                postsTotalReactions:"60"
+            },
+            zomato: {
+                dummyReviewText: "Good Restaurant",
+                totalReviews: "411",
+            },
+            tripAdvisor: {
+                rank: "54",
+                totalReview:"112",
+                positiveReview: "100",
+                negativeReview: "12"
+            }
+
+        },
+        
+
+    ]
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,101 +1,12 @@
 var searchData = {};
-var x = {};
-var fetchedData = [];
-var z = {};
-z['restaurants'] = [];
 $('document').ready(function () {
     makeTemplates();
-
+rb('.pageContainer .card','data',data.restaurantData);
 })
-bind('.button', function () {
-    console.log("Submit");
-    searchData.name = $('.restaurantName').val().trim();
 
-    execute('fetchData', searchData, function (data) {
-        // if (data.length != 0)
-        // data.totalEvent = (data.events.eventDetail.length);
+bind('.mainContainer .pageContainer .topBar .addButton',function(){
+    searchData.name = $('.mainContainer .pageContainer .topBar .searchField').val().trim();
+    execute('fetchData',searchData,function(data){
         console.log(data);
-        z.restaurants.push(data);
-        displayData(z)
     })
-
-
-    // execute('restaurantDetail', searchData, function(data) {
-    //     // data.totalEvent = (data.events.eventDetail.length);
-
-    //     // z.restaurants.push(data);
-    //     // console.log(z);
-    //     var y = {};
-    //     y.restaurants = data;
-    //     displayData(y)
-    // })
 })
-
-function displayData(data) {
-    console.log(data);
-
-
-    z = (data);
-
-
-    rb('.mainContainer', 'restaurants', z, {}, '.events', function (el, x) {
-        console.log(x.restaurantId);
-        var y = {};
-        y.name = x.restaurantId
-        execute('eventDetails', y, function (eventsData) {
-            console.log(eventsData);
-            var days = [];
-            for (var i = 0; i < 31; i++) {
-                days.push({
-                    day: i + 1
-                });
-            }
-            eventsData.days = days;
-            $('.eventContainerBlock').show();
-            rb('.eventContainerBlock', 'event', eventsData, {}, '.eventOverlay', function () {
-                $('.eventContainerBlock').hide();
-            })
-        })
-    });
-
-    bind('.post', function (data) {
-        var y = {};
-        // y.name = '';
-        if (typeof $(this).data('id') != "string")
-            y.name = JSON.stringify($(this).data('id'));
-        else
-            y.name = $(this).data('id')
-        console.log(y);
-        execute('postDetails', y, function (postDetails) {
-            // console.log(postDetails);
-            $('.eventContainerBlock').show();
-            rb('.eventContainerBlock', 'post', postDetails, {}, '.eventOverlay', function () {
-                $('.eventContainerBlock').hide();
-            })
-
-        })
-    })
-
-
-
-    bind('.addButton', function () {
-        $('.addRestaurantContainer').show();
-        rb('.addRestaurantContainer', 'addRestaurant', {}, {}, '.overlay', function () {
-            $('.addRestaurantContainer').hide();
-        });
-        bind('.add', function () {
-            $('.addRestaurantContainer').hide();
-
-            searchData.name = $('.addRestaurant').val().trim();
-            if (searchData.name) {
-                execute('fetchData', searchData, function (data) {
-                    // var p = {};
-                    // p.restaurant = data
-                    z.restaurants.push(data);
-                    console.log(data);
-                    displayData(z);
-                })
-            }
-        })
-    })
-}

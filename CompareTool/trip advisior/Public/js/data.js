70f6ab8685dffd4bbba48c4580ec62582a8eb92e
UtsavPreet <utsavpreet@gmail.com>
2017-08-13 15:07:20 +0530

    Added Zomato and TripAdvisor API

---
+++ CompareTool/trip advisior/Public/js/data.js
@@ -0,0 +1,23 @@
+var re = {
+    "restaurants": [{
+        "restaurant": {
+            "average_cost_for_two": "842",
+            "cuisines": "",
+            "id": "",
+            "name": "",
+            "user_rating": {
+                "aggregate_rating": "",
+                "rating_text": "",
+                "votes": ""
+            }
+        }
+    }]
+};
+var c = {
+    "competitorArray": [{
+        "restaurantName": "trend",
+        "competitor": [{
+            "name": "pier 38"
+        }]
+    }]
+}
--- server.js
+++ server.js
@@ -22,6 +22,7 @@
      console.log("connected");
      global.db = db;
  });
+ global.totalEvent = {};
  fb.setAccessToken('778609825679453|i_EEmwEy9_ZLUxcmnafb4-IuPXM');
  global.pageID;
 
@@ -29,24 +30,24 @@
      global.pageID = req.body.name;
      fb.api('', 'post', {
          batch: [{
-                 method: 'get',
-                 relative_url: global.pageID + '?fields=name_with_location_descriptor,picture,location,talking_about_count,checkins,fan_count,overall_star_rating,about,cover,feed{name,id,created_time,likes.limit(0).summary(true),comments.limit(0).summary(true),message.limit(0).summary(true),reactions.limit(0).summary(true),status_type,shares},events.limit(10){name,description,attending_count,cover,declined_count,start_time,interested_count}&since=2017-08-06&until=2017-08-07'
-             },
-         ]
+             method: 'get',
+             relative_url: global.pageID + '?fields=name_with_location_descriptor,picture,location,talking_about_count,checkins,fan_count,overall_star_rating,about,cover,feed{name,id,created_time,likes.limit(0).summary(true),comments.limit(0).summary(true),message.limit(0).summary(true),reactions.limit(0).summary(true),status_type,shares},events.limit(10){name,description,attending_count,cover,declined_count,start_time,interested_count}&since=2017-08-06&until=2017-08-07'
+         }, ]
      }, function (res) {
          res0 = JSON.parse(res[0].body);
          res0.feed.totalPost = res0.feed.data.length;
-         res0.events.totalEvent.count = res0.events.data.length;
+         totalEvent.count = res0.events.data.length;
          for (var i = 0; i < res0.feed.data.length; i++) {
              res0.feed.data[i].created_time = res0.feed.data[i].created_time.substr(0, 10);
          }
+         totalEvent.attendingCount = 0;
          for (var i = 0; i < res0.events.data.length; i++) {
              res0.events.data[i].start_time = res0.events.data[i].start_time.substr(0, 10);
              res0.events.data[i].date = res0.events.data[i].start_time.substr(8, 2);
              res0.events.data[i].month = res0.events.data[i].start_time.substr(5, 2);
-             totalEvent.attendingCount = res0.events.data[i].attending_count
-
+             global.totalEvent.attendingCount += res0.events.data[i].attending_count;
          }
+         res0.totalEvent = totalEvent;
          res0.restaurantId = global.pageID;
          global.db.collection('pages').save({
              _id: req.body.name,

2a23af8d243ff6614cd2f0969ef497a670c6d869
UtsavPreet <utsavpreet@gmail.com>
2017-08-28 20:43:40 +0530

    Integrated Google AutoComplete API

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,18 @@
 var searchData = {};
-var dbData =[];
+var dbData = [];
 $('document').ready(function () {
     makeTemplates();
 })
-bind('.mainContainer .topBar .addButton', function () {
+bind('.mainContainer .topBar .optionsContainer .add .addRestaurant', function () {
     searchData.zomato = $('.zomato').val().trim();
     searchData.facebook = $('.facebook').val().trim();
     searchData.tripAdvisor = $('.tripadvisor').val().trim();
     searchData.instagram = $('.instagram').val().trim();
-    searchData.google = $('.google').val().trim();
     $('.mainContainer .dataLoader').show();
     execute('fetchData', searchData, function (data) {
         console.log(data);
         rb('.mainContainer .card', 'data', data);
         $('.mainContainer .dataLoader').hide();
-        for (var i = 0; i < data.length; i++) {
-            dbData.push({name:data[i].facebook.name_with_location_descriptor});
-        }
-        console.log(dbData);
         screenBind();
     })
     console.log(searchData);
@@ -48,7 +43,23 @@ scrollVar.on('scroll', function () {
     lastX = currX;
 });
 
+bind('.google', function () {
+    console.log("Google");
+    $('.mainContainer .mapPopup').show();
+    initMap();
+})
+
 function screenBind() {
+    bind('.mainContainer .topBar .optionsContainer .refreshData .refreshRestaurant', function () {
+        $('.mainContainer .dataLoader').show();
+        execute('fetchData', searchData, function (data) {
+            console.log(data);
+            rb('.mainContainer .card', 'data', data);
+            $('.mainContainer .dataLoader').hide();
+            screenBind();
+        })
+        console.log(searchData);
+    })
     bind('.overlay', function () {
         $('.overlay').hide();
         $('.eventPopup').hide();
@@ -57,7 +68,9 @@ function screenBind() {
         $('.overlay').show();
         $('.eventPopup').show();
         $('dataLoader').show();
-        execute('getDetails',{dbData}, function (data) {
+        execute('getDetails', {
+            dbData
+        }, function (data) {
             console.log(data);
             var x = {
                 eventData: data,
@@ -77,3 +90,24 @@ function screenBind() {
         $(this).hide();
     })
 }
+
+function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat:28.6139, lng: 77.2090},
+          zoom: 13
+        });
+
+        var input = document.getElementById('google');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+        autocomplete.addListener('place_changed', function() {
+          
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+          searchData.google = place.place_id;
+          console.log(searchData);
+        });
+      }
